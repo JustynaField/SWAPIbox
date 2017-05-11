17ffdb8db152e917739b44234bb31a470d209a9e
Laura Turk <laura.lk.turk@gmail.com>
2017-05-11 10:59:38 -0600

    Crawl test

---
+++ src/App/stubs/crawl-stub.js
@@ -0,0 +1,59 @@
+module.exports = {
+title: "A New Hope",
+episode_id: 4,
+opening_crawl: "It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal secret plans to the Empire's ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet. Pursued by the Empire's sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy....",
+director: "George Lucas",
+producer: "Gary Kurtz, Rick McCallum",
+release_date: "1977-05-25",
+characters: [
+"http://swapi.co/api/people/1/",
+"http://swapi.co/api/people/2/",
+"http://swapi.co/api/people/3/",
+"http://swapi.co/api/people/4/",
+"http://swapi.co/api/people/5/",
+"http://swapi.co/api/people/6/",
+"http://swapi.co/api/people/7/",
+"http://swapi.co/api/people/8/",
+"http://swapi.co/api/people/9/",
+"http://swapi.co/api/people/10/",
+"http://swapi.co/api/people/12/",
+"http://swapi.co/api/people/13/",
+"http://swapi.co/api/people/14/",
+"http://swapi.co/api/people/15/",
+"http://swapi.co/api/people/16/",
+"http://swapi.co/api/people/18/",
+"http://swapi.co/api/people/19/",
+"http://swapi.co/api/people/81/"
+],
+planets: [
+"http://swapi.co/api/planets/2/",
+"http://swapi.co/api/planets/3/",
+"http://swapi.co/api/planets/1/"
+],
+starships: [
+"http://swapi.co/api/starships/2/",
+"http://swapi.co/api/starships/3/",
+"http://swapi.co/api/starships/5/",
+"http://swapi.co/api/starships/9/",
+"http://swapi.co/api/starships/10/",
+"http://swapi.co/api/starships/11/",
+"http://swapi.co/api/starships/12/",
+"http://swapi.co/api/starships/13/"
+],
+vehicles: [
+"http://swapi.co/api/vehicles/4/",
+"http://swapi.co/api/vehicles/6/",
+"http://swapi.co/api/vehicles/7/",
+"http://swapi.co/api/vehicles/8/"
+],
+species: [
+"http://swapi.co/api/species/5/",
+"http://swapi.co/api/species/3/",
+"http://swapi.co/api/species/2/",
+"http://swapi.co/api/species/1/",
+"http://swapi.co/api/species/4/"
+],
+created: "2014-12-10T14:23:31.880000Z",
+edited: "2015-04-11T09:46:52.774897Z",
+url: "http://swapi.co/api/films/1/"
+}
